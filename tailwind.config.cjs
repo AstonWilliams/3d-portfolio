40dfa56fc4587fa2c45ebe344effbf181865187f
Aston Williams <karlkuberx@gmail.com>
2025-03-18 22:28:13 +0500

    1.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('src/new_Assets/herobg.jpg')",
+        "hero-pattern": "url('src/assets/herobg.jpg')",
       },
     },
   },
