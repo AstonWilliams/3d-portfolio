5b917fe6c09982abaff647367204ad3464d9b14a
Aston Williams <karlkuberx@gmail.com>
2025-03-18 22:49:29 +0500

    1.1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,19 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section
+      className={`relative w-full h-screen mx-auto`}
+      style={{
+        backgroundImage: 'url("src/assets/herobg.jpg")', // Replace with your image path
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
