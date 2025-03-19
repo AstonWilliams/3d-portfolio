d32b96a6e69af1e315f6d545a742fe2509933099
Aston Williams <karlkuberx@gmail.com>
2025-03-19 06:56:48 +0500

    1.3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,18 @@ import herobg from "../assets/herobg.jpg"; // Import the image
 
 const Hero = () => {
   return (
-    <section
-      className={`relative w-full h-screen mx-auto`}
-      style={{
-        backgroundImage: `url(${herobg})`, // Use imported image
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+  <section
+    className="relative w-full h-screen mx-auto"
+    style={{
+      backgroundImage: 'url("src/assets/bg4.jpg")', // Use optimized WebP
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      imageRendering: 'auto', // Prevents pixelation
+      // filter: 'blur(0.8px)', // Slight blur to smooth rough edges (optional)
+    }}
+  >
+
       <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#AF01FA",
+        primary: "#060812",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
