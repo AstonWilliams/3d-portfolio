5d36420526f26a8d3f1554657b8054f94b822a4a
Aston Williams <karlkuberx@gmail.com>
2025-03-19 07:16:42 +0500

    1.3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,18 +5,14 @@ import herobg from "../assets/herobg.jpg"; // Import the image
 
 const Hero = () => {
   return (
-  <section
-    className="relative w-full h-screen mx-auto"
-    style={{
-      backgroundImage: 'url("src/assets/herobg.jpg")', // Use optimized WebP
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      imageRendering: 'auto', // Prevents pixelation
-      // filter: 'blur(0.8px)', // Slight blur to smooth rough edges (optional)
-    }}
-  >
-
+    <section
+      className={`relative w-full h-screen mx-auto`}
+      style={{
+        backgroundImage: `url(${herobg})`, // Use imported image
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
